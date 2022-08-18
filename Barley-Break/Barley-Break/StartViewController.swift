5c44c6ded4e65a8494b9ba6a1815b4b771e47d70
Oleksander Semeniuk <semenag01@meta.ua>
2022-08-18 15:16:21 +0300

    test run app

---
+++ Barley-Break/Barley-Break/StartViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StartViewController.swift
+//  Barley-Break
+//
+//  Created by Developer on 18.08.2022.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
